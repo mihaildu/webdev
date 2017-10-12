3142b520798ae0dc6d46bfaa903f0c9f6707bb24
Mihail Dunaev <mihail.dunaev91@gmail.com>
2017-10-13 02:04:48 +0300

    Add babeljs tests

---
+++ tests/frontend/js/es6/babeljs/test2/src/mod.js
@@ -0,0 +1,7 @@
+/* a simple function that prints a message */
+const test = (msg) => {
+    console.log(msg);
+};
+
+/* this is an ES6 feature */
+export default test;
---
+++ tests/frontend/js/es6/babeljs/test2/src/test1.js
@@ -0,0 +1,7 @@
+/*
+ * import is not yet implemented in node.js
+ * so running this directly with node will fail
+ * */
+import test from "./mod"
+
+test("hello");
