b78806814de249dd762145d45624d5669dafda90
Mihail Dunaev <mihail.dunaev@contentsquare.com>
2022-01-25 18:12:32 +0000

    add vue-router refs

---
+++ tests/frontend/js/vue/test5-router/src/router/index.ts
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    // redirect: arg => {
+    //   //return '/about';
+    // }
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  //console.log('route guard called before every route');
+  //console.log(to);
+  //console.log(from);
+  next();
+});
+
+export default router
---
+++ tests/frontend/js/vue/test5-router/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    Hi from Home!
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Route } from 'vue-router';
+//import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  beforeRouteEnter(to: Route, from: Route, next: Function) {
+    console.log('beforeRouteEnter in Home');
+    //next({ name: 'About' });
+    //next('/about');
+    next();
+  }
+})
+export default class Home extends Vue {
+
+  beforeMount() {
+    //console.log('hi');
+    //this.$router.push('/about');
+  }
+
+  created() {
+    //console.log('component created');
+  }
+
+  mounted() {
+    //console.log(this.$router);
+    //this.$router.push('/about');
+    //console.log(this.$route.params);
+  }
+}
+</script>
